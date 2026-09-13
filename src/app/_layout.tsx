c133833b2503c58fde1e5ac80a9e277491872b3e
Veera Bramha Chary <veerabramhachary2003@gmail.com>
2026-09-13 11:49:02 +0530

    feat: add queryClient setup and Tailwind CSS configuration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,12 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { queryClient } from "../lib/queryClient";
 
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Stack screenOptions={{ headerShown: false }} />
+        </QueryClientProvider>
+    );
 }
